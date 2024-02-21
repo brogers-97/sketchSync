e04a9e06e012eccbd514fa5745fdc482b8ef375a
brogers-97 <brburr24@gmail.com>
2024-02-21 13:30:11 -0700

    fixed canvas sizing, added toolbar

--- src/App.js
+++ src/App.js
@@ -5,62 +5,56 @@ import './App.css';
 function App() {
   const [isDrawing, setIsDrawing] = useState(false)
   const canvasRef = useRef(null)
+  const isDrawingRef = useRef(isDrawing);
   
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d')
 
+    //MAIN DRAW FUNCTIONS
 
-    const resizeCanvas = () => {
-      const {width, height} = canvas.getBoundingClientRect()
-
-      const dpr = window.devicePixelRatio.devicePixelRatio || 1;
-      canvas.width = width * dpr
-      canvas.height = height * dpr
-      canvas.style.width = `${width}px`
-      canvas.style.height = `${height}px`
-
-      context.scale(dpr, dpr)
-    }
-
-    resizeCanvas()
-    window.addEventListener('resize', resizeCanvas)
-
-
-
-    const startDrawing = (e) => {
-      const rect = canvas.getBoundingClientRect();
-      const x = e.clientX - rect.left
-      const y = e.clientY - rect.top
-      console.log(x,y)
+    function startDrawing(e) {
+      console.log('start')
+      
+      isDrawingRef.current = true
       context.beginPath()
-      context.moveTo(x,y)
-      setIsDrawing(true)
     }
-
-    const draw = (e) => {
-      if (!isDrawing) return;
-      const rect = canvas.getBoundingClientRect();
-      const x = e.clientX - rect.left
-      const y = e.clientY - rect.top
-      console.log(x,y)
+    function endDrawing(e) {
+      console.log('end')
+      
+      isDrawingRef.current = false
+    }
+    function getMousePos(canvas, evt) {
+      var rect = canvas.getBoundingClientRect(),
+      scaleX = canvas.width / rect.width,
+      scaleY = canvas.height / rect.height;
+      
+      return {
+        x: (evt.clientX - rect.left) * scaleX,
+        y: (evt.clientY - rect.top) * scaleY
+      }
+    }
+    function draw(e) {
+      if(!isDrawingRef.current) return;
+      console.log('drawing')
+      let {x,y} = getMousePos(canvas, e)
       context.lineTo(x,y)
       context.stroke()
     }
-
-    const stopDrawing = () => {
-      context.closePath()
-      setIsDrawing(false)
-    }
-
-    canvas.addEventListener('mousedown', startDrawing)
-    canvas.addEventListener('mousemove', draw)
-    canvas.addEventListener('mouseup', stopDrawing)
-    canvas.addEventListener('mouseleave', stopDrawing)
+    
+    window.addEventListener("mousedown", startDrawing)
+    window.addEventListener("mouseup", endDrawing)
+    window.addEventListener("mousemove", draw)
     
   }, [])
 
+
+
+
+
+
+  //TOOLBAR FUNCTIONS
 
   const clearCanvas = () => {
     const canvas = canvasRef.current;
@@ -68,10 +62,14 @@ function App() {
     context.clearRect(0,0,canvas.width,canvas.height)
   }
   const drawRect = () => {
+    console.log('click')
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d')
     context.rect(20,20,100,100)
     context.fill()
+  }
+  const Click = () => {
+    console.log('click')
   }
 
 
@@ -81,6 +79,7 @@ function App() {
       <div className='toolbar'>
         <button onClick={clearCanvas}>Clear</button>
         <button onClick={drawRect}>draw</button>
+        <button onClick={Click}>Click me</button>
       </div>
     </div>
   )
